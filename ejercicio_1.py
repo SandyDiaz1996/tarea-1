57894b3322da8f7cc7351ac76ab155a54ea185d8
Sandy Diaz <sdiazo@unemi.edu.ec>
2021-06-11 12:39:45 -0500

    Tarea Completa

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -5,3 +5,11 @@ class Fraccion:
     
     def mostrar(self):
         return 'Fracción: {}/{}.'.format(self.numera, self.denomi)
+
+fraccion1 = Fraccion(5, 8)
+fraccion2 = Fraccion(10, 11)
+fraccion3 = Fraccion(20, 20)
+print('__FRACCIONES__')
+print(fraccion1.mostrar())
+print(fraccion2.mostrar())
+print(fraccion3.mostrar())
